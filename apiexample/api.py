76ad09d67ff3029a8c16dbd180d46f4d798c12e3
Shantanu Pandey <shantanu.pandey@live.in>
2022-02-28 21:28:14 +0530

    Create api.py

---
+++ apiexample/api.py
@@ -0,0 +1,21 @@
+@app.route('/')
+def helloworld():
+    return "bye bye world"
+
+@app.route('/home')
+def homefunction():
+    filedata = open('home.html','rt').read()
+    return filedata
+
+@app.route('/sum/<n>/<m>')
+def sumfunc(n, m):
+    # num1,num2-> strings
+    n1 = int(n)
+    n2 = int(m)
+    return str(n1+n2)
+
+# 2 options for host-> if you leave this blank, app runs on LOCALHOST (only inside the machine)
+# host=0.0.0.0 -> app runs outside the machine 
+app.run(host="0.0.0.0")
+
+                                                                                  14,24         Bot
